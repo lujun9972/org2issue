910b98c858762fd14b11d261626c5e979dde0833
darksun <lujun9972@gmail.com>
2019-05-31 17:41:29 +0800

    update

--- org2issue.el
+++ org2issue.el
@@ -202,6 +202,34 @@ will return \"this is title\" if OPTION is \"TITLE\""
     (push "\"" res)
     (apply #'concat "\"" (nreverse res))))
 
+(defun org2issue--convert-src (body &optional filename)
+  (let ((filename (or filename (buffer-file-name))))
+    (with-temp-buffer
+      (insert body)
+      (goto-char (point-min))
+      (while (re-search-forward
+;;; TODO: not only links need to convert, but also inline
+;;; images, may add others later
+              ;; "<a[^>]+href=\"\\([^\"]+\\)\"[^>]*>\\([^<]*\\)</a>" nil t)
+              "<[a-zA-Z]+[^/>]+\\(src\\|href\\)=\"\\([^\"]+\\)\"[^>]*>" nil t)
+        (let* ((root-path (vc-git-root filename))
+               (asset-path (match-string 2))
+               (asset-path-begin (match-beginning 2))
+               (asset-path-end (match-end 2))
+               (asset-abs-path (expand-file-name asset-path (file-name-directory filename)))
+               (asset-relative-path (file-relative-name asset-abs-path root-path)))
+          (unless (url-type (url-generic-parse-url asset-path)) ;; 判断是否为绝对路径的URI
+            (if (not (file-exists-p asset-abs-path))
+                (message "ORG2ISSUE: [WARN] File %s in hyper link does not exist, org file: %s." asset-abs-path filename)
+              (let ((converted-path
+                     (with-temp-buffer
+                       (insert (vc-git-dir-extra-headers default-directory))
+                       (re-search-backward "^Remote[[:blank:]]+:[[:blank:]]+\\([^[:blank:]\r\n]+\\)")
+                       (match-string 1))
+                     (concat "/" (file-relative-name pub-abs-path pub-root-dir)))) ;TODO 转换后的地址
+                (setf (buffer-substring asset-path-begin asset-path-end) converted-path))))))
+      (buffer-string))))
+
 ;;;###autoload
 (defun org2issue (&optional delete)
   (interactive "P")
